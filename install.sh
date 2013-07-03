b6d34ce8398081860a92d2fedcebff93d6241231
Sebastian Gun <sebastian.gun@mercadolibre.com>
2013-07-03 11:19:16 -0300

    Fixed gitconfig path

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ ln -svF $PWD/vimrc ~/.vimrc
 ln -svF $PWD/bash_profile ~/.bash_profile
 ln -svF $PWD/git-completion.bash ~/.git-completion.bash
 ln -svF $PWD/git-prompt.sh ~/.git-prompt.sh
-ln -svF $PWD/gitconfig.sh ~/.gitconfig.sh
+ln -svF $PWD/gitconfig ~/.gitconfig
 
 echo "DONE!"
