7285328b7ae388e7c1348997a8951d35f507d15a
Sebastian Gun <sebastian.gun@mercadolibre.com>
2013-07-08 11:51:55 -0300

    All settings moved to user settings file, to avoid override them during program updates

--- osx/install.sh
+++ osx/install.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 ln -svF $PWD/DefaultKeyBinding.dict ~/Library/KeyBindings/DefaultKeyBinding.dict
-ln -svF $PWD/Preferences.sublime-settings.Default ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/Default/Preferences.sublime-settings
 ln -svF $PWD/Preferences.sublime-settings.User ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings
 
 echo "DONE!"
