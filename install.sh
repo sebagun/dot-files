06c8daaebd824a1153cf4e0319635f5b4d4e8d6a
Sebastian Gun <sebastian.gun@mercadolibre.com>
2013-08-17 23:24:54 -0300

    Added gitk and kdiff3 settings files

--- install.sh
+++ install.sh
@@ -5,5 +5,7 @@ ln -svF $PWD/bash_profile ~/.bash_profile
 ln -svF $PWD/git-completion.bash ~/.git-completion.bash
 ln -svF $PWD/git-prompt.sh ~/.git-prompt.sh
 ln -svF $PWD/gitconfig ~/.gitconfig
+ln -svF $PWD/gitk ~/.gitk
+ln -svF $PWD/kdiff3rc ~/.kdiff3rc
 
 echo "DONE!"
