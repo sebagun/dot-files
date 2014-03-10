96da461fdfb077e4857ec66348111e0c3b77e640
Sebastian Gun <sebastian.gun@mercadolibre.com>
2014-03-10 17:19:38 -0300

    Some nice hacks for OS X

--- osx/install.sh
+++ osx/install.sh
@@ -3,4 +3,10 @@
 ln -svF $PWD/DefaultKeyBinding.dict ~/Library/KeyBindings/DefaultKeyBinding.dict
 ln -svF $PWD/Preferences.sublime-settings.User ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings
 
+### Nice hacks ###
+# http://apple.stackexchange.com/questions/71939/how-to-get-keyboard-focus-on-login-screen-password-field-in-os-x-10-8
+defaults write ~/Library/Preferences/com.apple.screensaver loginWindowIdleTime 10800
+# http://osxdaily.com/2012/06/08/14-must-know-tips-tricks-for-mac-os-x/
+chflags nohidden ~/Library/
+
 echo "DONE!"
