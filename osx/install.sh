39aeaed36679d6ab19e441241475e013284f270a
Sebastian Gun <sebastian.gun@mercadolibre.com>
2014-10-27 09:15:26 -0300

    Some OS X tweaks

--- osx/install.sh
+++ osx/install.sh
@@ -10,4 +10,14 @@ defaults write ~/Library/Preferences/com.apple.screensaver loginWindowIdleTime 1
 # http://osxdaily.com/2012/06/08/14-must-know-tips-tricks-for-mac-os-x/
 chflags nohidden ~/Library/
 
+### https://github.com/mathiasbynens/dotfiles/blob/master/.osx ###
+# Disable the “Are you sure you want to open this application?” dialog
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+# Disable the crash reporter
+defaults write com.apple.CrashReporter DialogType -string "none"
+# Reveal IP address, hostname, OS version, etc. when clicking the clock in the login window
+sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName
+# Avoid creating .DS_Store files on network volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
 echo "DONE!"
